518cb5ddd526be0c9f9137561fe67b6a0e4626d4
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-12-04 11:03:03 +0300

    edit comments in main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-function(showOutput){
-
+//1. Get user input from HTML
+function user_Output() {
+//2. Declare all variables
 var a = parseInt(document.getElementById("length a").value);
 var b = parseInt(document.getElementById("length b").value);
 var c = parseInt(document.getElementById("length c").value);
+
+var clickMe = document.getElementById("btn").value;
+
+//2. Push inputs into an array
+
+var manArray = [];
+manArray.push(a, b, c);
+
+//3. Test properties
 
 if (a + b > c || b + c > a || b + c > a) {
   else if (a === b && b === c && a === c) {
     document.getElementById("button").innerHTML = "Equilateral, guilty as charged!";
   }
-  else if a !== b and b !== c and a !== c {
-    document.getElementById("button").innerHTML = "Scalene, guilty as charged!";
+  else if (a === b || b === c || c === a) {
+    document.getElementById("button").innerHTML = "Isosceles, guilty as charged!";
   }
   else {
     document.getElementById("button").innerHTML = "Scalene, guilty as charged!";
   }
+}
 };
