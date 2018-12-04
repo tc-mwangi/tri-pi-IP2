c4c66ecb428791bcb046fe52fed611a4bc309192
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-12-04 13:17:09 +0300

    add new function and feed arguments a, b and c.

--- js/main.js
+++ js/main.js
@@ -5,24 +5,29 @@ var a = parseInt(document.getElementById("length a").value);
 var b = parseInt(document.getElementById("length b").value);
 var c = parseInt(document.getElementById("length c").value);
 
-var clickMe = document.getElementById("btn").value;
+var clickMe = document.getElementById("clickMe").value;
 
 //2. Push inputs into an array
 
 var manArray = [];
 manArray.push(a, b, c);
-
+act();
+}
 //3. Test properties
-
+function act(a,b,c) {
 if (a + b > c || b + c > a || b + c > a) {
-  else if (a === b && b === c && a === c) {
-    document.getElementById("button").innerHTML = "Equilateral, guilty as charged!";
+
+  if (a === b && b === c && a === c) {
+    clickMe.innerHTML = "<p>Equilateral, guilty as charged!</p>"
   }
   else if (a === b || b === c || c === a) {
-    document.getElementById("button").innerHTML = "Isosceles, guilty as charged!";
+    clickMe.innerHTML = "<p>Isosceles, guilty as charged!.</p>"
   }
   else {
-    document.getElementById("button").innerHTML = "Scalene, guilty as charged!";
+    clickMe.innerHTML = "<p>Scalene, guilty as charged!.</p>"
   }
 }
-};
+else {
+    clickMe.innerHTML = "<p>This is not a triangle.</p>"
+}
+}
